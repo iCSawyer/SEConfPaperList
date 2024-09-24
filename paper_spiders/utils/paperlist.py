1eeaa67c2154296bdef7b12ec13721cda7da26fb
lihuan <lihuan0530@gmail.com>
2024-09-24 15:08:29 +0800

    add ASE 2024

--- paper_spiders/utils/paperlist.py
+++ paper_spiders/utils/paperlist.py
@@ -23,6 +23,7 @@ paper_list = [
     {"conf": "ICSE 2024", "url": "https://conf.researchr.org/track/icse-2024/icse-2024-research-track"},
     {"conf": "FSE 2024", "url": "https://2024.esec-fse.org/track/fse-2024-research-papers"},
     {"conf": "ISSTA 2024", "url": "https://2024.issta.org/track/issta-2024-papers"},
+    {"conf": "ASE 2024", "url": "https://conf.researchr.org/track/ase-2024/ase-2024-research"},
 ]
 
 paper_url_list = [p["url"] for p in paper_list]
