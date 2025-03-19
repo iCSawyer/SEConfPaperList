9565ec3bfd794c4cb2e99f6ab67d063a28b0bb06
lihuan <lihuan0530@gmail.com>
2025-03-19 20:24:53 +0800

    add icse 2025

--- paper_spiders/utils/paperlist.py
+++ paper_spiders/utils/paperlist.py
@@ -24,6 +24,8 @@ paper_list = [
     {"conf": "FSE 2024", "url": "https://2024.esec-fse.org/track/fse-2024-research-papers"},
     {"conf": "ISSTA 2024", "url": "https://2024.issta.org/track/issta-2024-papers"},
     {"conf": "ASE 2024", "url": "https://conf.researchr.org/track/ase-2024/ase-2024-research"},
+    # 2025
+    {"conf": "ICSE 2025", "url": "https://conf.researchr.org/track/icse-2025/icse-2025-research-track"},
 ]
 
 paper_url_list = [p["url"] for p in paper_list]
